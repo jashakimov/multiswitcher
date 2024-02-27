a03c7344c07c6f13fa907cdb43ee2bb65e0fb899
Jas Hakimov <jasforever1@gmail.com>
2024-02-27 17:06:22 +0300

    finish test service

--- internal/service/filter/switcher.go
+++ internal/service/filter/switcher.go
@@ -43,7 +43,7 @@ func TurnOnAutoSwitch(statManager statistic.Service, info Filter) {
 	for {
 		select {
 		case <-t.C:
-			bytes, err := statManager.GetBytesByIP(info.DstIP)
+			bytes, err := statManager.GetBytesByIP(info.MasterIP)
 			if err != nil {
 				log.Println(err)
 				continue
